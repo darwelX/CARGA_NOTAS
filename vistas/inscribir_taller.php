bf0a56ef872b7a5da6e0307aafc1424f54cd61ca
Darwel Quintero <darwel34@gmail.com>
2013-09-10 11:45:58 -0430

    se adapto la vista para inscribir talleres

--- vistas/inscribir_taller.php
+++ vistas/inscribir_taller.php
@@ -8,10 +8,12 @@
   <script type="text/javascript" src="../js/jquery.js"></script>
   <script type="text/javascript" src="../js/jquery.ketchup.all.min.js"></script>
   <script type="text/javascript">
-  function validateForm(formObj) {  
-      $("#yourSubmitId").html("Porfavor Espere..")
-      formObj.procesarButton.disabled = true;  
-      return true;  
+  function validateForm(formObj) {
+	  if($('#form1').ketchup('isValid')){
+      	$("#yourSubmitId").html("Porfavor Espere..")
+      	formObj.procesarButton.disabled = true;  
+      	return true; 
+	  } 
 
   }
   </script>
@@ -24,11 +26,27 @@
   <fieldset style="width: 50%">
   <legend>Crear Taller</legend>
   <table>
+
+    <tr>
+    	<th colspan="8" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; font-weight: bold; color: red;">
+  		        	<?php 
+		        		if(isset($mensaje)){
+							echo $mensaje;
+                        }
+					?>  	
+    	</th>
+    </tr>
     
     <tr>
-        <td><label>Cantidad de Evaluaciones:</label></td>
-        <td><input type="text" name="cantidad" id="cantidad" data-validate="validate(required,number,min(4),max(10))" /></td>
+        <td><label>C&oacute;digo:</label></td>
+        <td><input type="text" name="codigo" id="codigo" data-validate="validate(required,number,minlength(3),maxlength(5))" value="<?php echo $_POST['codigo'];?>"/></td>
     </tr>
+    
+    <tr>
+        <td><label>Descripci&oacute;n:</label></td>
+        <td><input type="text" name="descripcion" id="descripcion" data-validate="validate(required)" value="<?php echo $_POST['descripcion'];?>"/></td>
+    </tr>
+        
     <tr>
       <td colspan="2" align="center">
         <input type="hidden" name="procesar" value="Guardar"/>
