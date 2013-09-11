4154cec1d7f9c4a8bd09d010606d2d49dc00aa72
Darwel Quintero <darwel34@gmail.com>
2013-09-11 14:16:36 -0430

    se agrego a logica que maneja la accion de insertar y actualizar

--- controllers/controller.taller.php
+++ controllers/controller.taller.php
@@ -5,7 +5,7 @@ require_once '../classes/class.util.Convert.php';
 
 $taller = new Taller();
 $mensaje="";
-
+$buscar = true;
 if(isset($_POST)){
 	
 	if(isset($_POST['procesar']) && $_POST['procesar'] == "Guardar"){
@@ -17,18 +17,40 @@ if(isset($_POST)){
 		}else{
 			$mensaje="El taller ya existe";
 		}
-		
+		require_once '../vistas/inscribir_taller.php';
 	}
 	
 	if(isset($_POST['procesar']) && $_POST['procesar'] == "Actualizar"){
-		
+		$taller->setId($_POST['id']);
+		$taller->setCodigo($_POST['codigo']);
+		$taller->setDescripcion($_POST['descripcion']);
+		if($taller->update()){
+			$mensaje="Taller acctualizado";
+			unset($buscar);
+			unset($_POST['id']);
+			unset($_POST['codigo']);
+			unset($_POST['descripcion']);
+			unset($buscar);
+		}
+		require_once '../vistas/actualizar_taller.php';
 	}
 	
 	if(isset($_POST['procesar']) && $_POST['procesar'] == "Buscar"){
+		$taller->setCodigo($_POST['codigo']);
+		if($taller->findBy("CODIGO = ".$taller->getCodigo())){
+			$buscar=false;
+			$_POST['codigo']=$taller->getCodigo();
+			$_POST['descripcion']=$taller->getDescripcion();
+			
+		}else{
+			$mensaje="Taller no encontrado";
+			unset($buscar);
+		}
+		require_once '../vistas/actualizar_taller.php';
 		
 	}
 	
-	require_once '../vistas/inscribir_taller.php';
+	
 }
 
 
