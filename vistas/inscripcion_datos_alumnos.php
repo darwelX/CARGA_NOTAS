2cc214e040c9929e8ed4d6ece6b8f0841c99317c
Darwel Quintero <darwel34@gmail.com>
2013-09-11 21:15:49 -0430

    se creo la vista que pide los datos para hacer una inscripcion

---
+++ vistas/inscripcion_datos_alumnos.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Carga Evaluaciones</title>
+  <link rel="stylesheet" type="text/css" href="../css/main.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/jquery.ketchup.css" media="screen" />
+  <script type="text/javascript" src="../js/jquery.js"></script>
+  <script type="text/javascript" src="../js/jquery.ketchup.all.min.js"></script>
+  <script type="text/javascript">
+  function validateForm(formObj) {
+	  if($('#form1').ketchup('isValid')){
+      	$("#yourSubmitId").html("Porfavor Espere..")
+      	formObj.procesarButton.disabled = true;  
+      	return true; 
+	  } 
+
+  }
+  </script>
+  
+</head>
+<body>
+    
+<!-- ../controllers/controller.cargaNota.php -->
+<form action="../controllers/controller.inscripcion.php" method="post" id="form1"  onsubmit="return validateForm(this);">
+  <fieldset style="width: 50%">
+  <legend>Busccar Estudiante</legend>
+  <table>
+
+    <tr>
+    	<th colspan="8" style="font-family: Arial, Helvetica, sans-serif; font-size: 16px; font-weight: bold; color: red;">
+  		        	<?php 
+		        		if(isset($mensaje)){
+							echo $mensaje;
+                        }
+					?>  	
+    	</th>
+    </tr>
+    
+    <tr>
+        <td><label>Cedula:</label></td>
+        <td><input type="text" name="cedula" id="cedula" data-validate="validate(required)" value="<?php echo (isset($_POST['cedula']))?$_POST['cedula']:'';?>"/></td>
+    </tr>
+    
+       
+    <tr>
+      <td colspan="2" align="center">
+        <input type="hidden" name="procesar" value="Buscar"/>
+    	<button type="submit" name="procesarButton" value="Busccar" id="yourSubmitId" onclick="$('#form1').ketchup();">
+    		<img alt="" src="../img/enviar.png" width="16" height="16"/>
+    		Buscar
+    	</button><!-- onclick="javascript: form.action='../controllers/controller.cargaNota.php';" -->
+      </td>
+    </tr>
+    
+  </table>
+  </fieldset>
+   
+</form>
+</body>
+</html>
