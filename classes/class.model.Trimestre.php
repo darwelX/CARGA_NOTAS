982c988694805cb4eeb727af7d4a39d0869470bb
Darwel Quintero <darwel34@gmail.com>
2013-09-12 17:56:51 -0430

    se agrego la clase que representa los trimestres

---
+++ classes/class.model.Trimestre.php
@@ -0,0 +1,53 @@
+<?php
+require_once '../classes/interface.Operaciones.php';
+require_once '../classes/class.model.Connect.php';
+
+class Trimestre implements Operaciones{
+
+	private $numero;
+	private $conexion;
+	private $sqlAll = "SELECT TRIMESTRE FROM ESTUDIANTES_HORARIOS WHERE TRIMESTRE IS NOT NULL GROUP BY TRIMESTRE ORDER BY TRIMESTRE";
+	private static $table = "ESTUDIANTES_HORARIOS";
+
+	public function __construct(){
+		$this->conexion = new Connect();
+	}
+
+	public function getNumero(){
+		return $this->numero;
+	}
+
+	public function setNumero($numero){
+		$this->numero = $numero;
+	}
+
+	public function find($id){
+
+	}
+
+	public function findAll(){
+		$this->conexion->conectar();
+		$this->stmt = $this->conexion->ejecutar($this->sqlAll);
+		$indice = 0;
+		$array=[];
+		while ($rsd=$this->conexion->obtener_filas($this->stmt)){
+			$trimestre=new Trimestre();
+			$trimestre->numero = $rsd['TRIMESTRE'];
+			$array[$indice]=$trimestre;
+			$indice++;
+			//return true;
+		}
+		return $array;
+
+	}
+
+	public function insert(){
+
+	}
+
+	public function update(){
+	}
+
+	public function findBy($condicion){
+	}
+}
