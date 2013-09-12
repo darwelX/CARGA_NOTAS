092466e7cecf862a374a855509bbea2888927f9c
Darwel Quintero <darwel34@gmail.com>
2013-09-12 18:08:28 -0430

    se agrego la busqueda de lapso y materias para la inscripcion

--- controllers/controller.inscripcion.php
+++ controllers/controller.inscripcion.php
@@ -1,20 +1,38 @@
 <?php
 require_once '../classes/class.model.Connect.php';
 require_once '../classes/class.model.Estudiante.php';
+require_once '../classes/class.model.Materia.php';
+require_once '../classes/class.model.Lapso.php';
 require_once '../classes/class.util.Convert.php';
 
 $estudiante = new Estudiante();
+$materias = [];
+$lapso = new Lapso();
 $convert = new Convert();
 $mensaje="";
 $buscar = true;
 if(isset($_POST)){
-
+    
 	if(isset($_POST['id_estudiante'])){
 		$estudiante->find($_POST['id_estudiante']);
 	}
-	
-	if(isset($_POST['procesar']) && $_POST['procesar'] == "Guardar"){
 
+	if(isset($_POST['lapso'])){
+		$lapso->find($_POST['lapso']);
+	}
+	
+	if(isset($_POST['trimestre']) || $_POST['trimestre'] != ""){
+		$materia = new Materia();
+		$materias = $materia->findMateriasPorTrimestreYCarrera($_POST['trimestre'], $estudiante->carrera->getId());		
+	}
+	
+	if(isset($_POST['procesar']) && $_POST['procesar'] == "Agregar Unidad"){
+		
+		if( !isset($_POST['trimestre']) || $_POST['trimestre'] == "" ){
+			$mensaje="Seleccione un Trimestre<br>";
+		}else{
+			
+		}
 	}
 
 	if(isset($_POST['procesar']) && $_POST['procesar'] == "Actualizar"){
@@ -26,7 +44,6 @@ if(isset($_POST)){
 		
 		if($estudiante->findBy("CEDULA = ".$estudiante->getCedula())){
 			$_POST['id_estudiante'] = $estudiante->getId();
-			require_once '../vistas/inscripcion_alumno_regular.php';
 			
 		}else{
 			$mensaje="Estudiante no encontrado";
@@ -37,6 +54,6 @@ if(isset($_POST)){
 
 	}
 
-
+	require_once '../vistas/inscripcion_alumno_regular.php';
 }
 ?>
