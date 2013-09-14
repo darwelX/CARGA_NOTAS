6c1fe6491916ef4f46c98d5fd1ac1ad78696f2f1
Darwel Quintero <darwel34@gmail.com>
2013-09-13 22:29:44 -0430

    se agregaron las librerias y css para el uso del framework bootstrap

--- vistas/index.php
+++ vistas/index.php
@@ -1,6 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap-responsive.css" media="screen" />
+<script type="text/javascript" src="../js/bootstrap.min.js"></script>
+<script type="text/javascript" src="../js/twitter-bootstrap-hover-dropdown.min.js"></script>
+<script type="text/javascript" src="../js/jquery.js"></script>
+<link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" media="screen" />
 <meta charset="ISO-8859-1">
 <title></title>
   <link rel="stylesheet" type="text/css" href="../css/main.css" media="screen" />
