6b135d460cb86d8836a1aaabd50922b6502c1b35
Darwel <darwel34@gmail.com>
2013-09-23 11:56:35 -0430

    se comento una linea

--- classes/class.model.Estudiante.php
+++ classes/class.model.Estudiante.php
@@ -70,6 +70,7 @@ class Estudiante implements Operaciones{
 	
 	public function find($id){
 		$this->conexion->conectar();
+		//echo $this->sqlAll." WHERE IDPERSONA = ".$id." AND PROCESO=3";
 		$this->stmt = $this->conexion->ejecutar($this->sqlAll." WHERE IDPERSONA = ".$id." AND PROCESO=3");
 		while ($rsd=$this->conexion->obtener_filas($this->stmt)){
 			$this->id = $rsd['IDPERSONA'];
