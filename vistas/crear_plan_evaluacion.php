16e5daa039e1096f795c16638a2ee0a9222a0880
Darwel Quintero <darwel34@gmail.com>
2013-09-25 16:53:18 -0430

    se comento el css anterior para que trabaje con el css de bootstrap y ademaas se agregaron las lineas del css y js de bootstrap

--- vistas/crear_plan_evaluacion.php
+++ vistas/crear_plan_evaluacion.php
@@ -3,12 +3,15 @@
 <head>
 <meta charset="ISO-8859-1">
 <title>Carga Evaluaciones</title>
-  <link rel="stylesheet" type="text/css" href="../css/main.css" media="screen" />
+  <!--<link rel="stylesheet" type="text/css" href="../css/main.css" media="screen" />-->
   <link rel="stylesheet" type="text/css" href="../css/jquery.ketchup.css" media="screen" />
+  <link rel="stylesheet" type="text/css" href="../css/bootstrap.min.css" media="screen" />
+  <script type="text/javascript" src="../js/bootstrap.min.js"></script>  
   <script type="text/javascript" src="../js/jquery.js"></script>
   <script type="text/javascript" src="../js/jquery.ketchup.all.min.js"></script>
   <script type="text/javascript">
-  function validateForm(formObj) {  
+  function validateForm(formObj) { 
+	  aler('hola') 
 	  if($('#form1').ketchup('isValid')){
       	$("#yourSubmitId").html("Porfavor Espere..")
       	formObj.procesarButton.disabled = true;  
