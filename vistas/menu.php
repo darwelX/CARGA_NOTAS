9805d8ddde305d68e2bdafcc8fc8faef280468f7
Darwel Quintero <darwel34@gmail.com>
2013-09-26 17:10:50 -0430

    se agregaron las restricciones por nivel de usuario asi como la opcion nueva de setear password ademas se cambio la cedula por la variable guardada en sesion

--- vistas/menu.php
+++ vistas/menu.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html>
 <head>
@@ -15,16 +16,19 @@
   <link rel="stylesheet" type="text/css" href="css/main.css" media="screen" />
 </head>
 <body>
+    <?php if($_SESSION['NIVEL']==5 || $_SESSION['NIVEL']==1){?>
 	<div class="btn-group">
 	    <button class="btn btn-primary dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Notas <span class="caret"></span></button>
 		<ul class="dropdown-menu">
 			<!-- dropdown menu links -->
-    		<li><a href="../vistas/crear_plan_evaluacion.php?&cedula=<?php echo $docente->getCedula();?>">Cargar Plan De Evaluacion</a></li>
-    		<li><a href="../vistas/imprimir_planilla_notas.php?&cedula=<?php echo $docente->getCedula();?>">Imprimir Planilla de Notas</a></li>
-    		<li><a href="../vistas/cargar_notas.php?&cedula=<?php echo $docente->getCedula();?>">Cargar Notas</a></li>
+    		<li><a href="../vistas/crear_plan_evaluacion.php?&cedula=<?=$_SESSION['CEDULA']?>">Cargar Plan De Evaluacion</a></li>
+    		<li><a href="../vistas/imprimir_planilla_notas.php?&cedula=<?=$_SESSION['CEDULA']?>">Imprimir Planilla de Notas</a></li>
+    		<li><a href="../vistas/cargar_notas.php?&cedula=<?=$_SESSION['CEDULA']?>">Cargar Notas</a></li>
 		</ul>
 	</div>
+	<?php }?>
 	
+	<?php if($_SESSION['NIVEL']==1){?>
 	<div class="btn-group">
 	    <button class="btn btn-danger dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Talleres <span class="caret"></span></button>
 		<ul class="dropdown-menu">
@@ -33,7 +37,9 @@
     		<li><a href="../vistas/actualizar_taller.php">Actualizar Taller</a></li>
 		</ul>
 	</div>
-	
+<?php }?>
+
+<?php if($_SESSION['NIVEL']==1){?>
 	<div class="btn-group">
 	    <button class="btn btn-warning dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Inscripciones <span class="caret"></span></button>
 		<ul class="dropdown-menu">
@@ -42,10 +48,21 @@
     		<li><a href="#">Nuevo Ingreso</a></li>
 		</ul>
 	</div>
-	
+<?php }?>	
+
+
+<?php if($_SESSION['NIVEL']==1){?>
+	<div class="btn-group">
+	    <button class="btn btn-success dropdown-toggle" data-toggle="dropdown" data-hover="dropdown">Usuarios <span class="caret"></span></button>
+		<ul class="dropdown-menu">
+			<!-- dropdown menu links -->
+    		<li><a href="../vistas/setear_password_profesores.php">Setear el password de los profesores</a></li>
+		</ul>
+	</div>
+<?php }?>
 	
     <div class="btn-group">
-		<a class="btn btn-inverse dropdown-toggle" data-toggle="dropdown" href="../vistas/login.php"> Salir
+		<a class="btn btn-inverse dropdown-toggle" data-toggle="dropdown" href="../controllers/controller.salir.php"> Salir
 			
 		</a>
 	</div>
