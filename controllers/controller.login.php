a32bdf4997ba4e74c266533766d172d917781a57
Darwel Quintero <darwel34@gmail.com>
2013-09-26 16:46:29 -0430

    se agrego la logica para autenticar el usuario asi como la de setear el password de los profesores ademas de la varible que controla el tipo de mensaje

--- controllers/controller.login.php
+++ controllers/controller.login.php
@@ -2,21 +2,44 @@
 require_once '../classes/class.model.Connect.php';
 require_once '../classes/class.model.Docente.php';
 require_once '../classes/class.model.Seccion.php';
+require_once '../classes/class.model.Usuario.php';
 $mensaje="";
-$docente = new Docente();
+$tipo_msg="";
+$usuario = new Usuario();
+
+
 if($_POST['procesar'] == "Iniciar"){
 	
-	$cedula = intval($_POST['cedula']);
+	$login = $_POST['login'];
+	$password= $_POST['password'];
+	
 	//$docente->setCedula($cedula);
-	if($docente->findBy("CEDULA = $cedula")){
+	if($usuario->findBy("LOGIN = '$login' AND PASS = '$password'")){
+		session_start();
+		$_SESSION['CEDULA']=$usuario->getCedula();
+		$_SESSION['NIVEL']=$usuario->getNivel();
+		$_SESSION['LOGIN']=$usuario->getLogin();
 		require_once '../vistas/index.php';
 	}else{
-		$mensaje="Profesor no encontrado";
+		$tipo_msg="error";
+		$mensaje="Errro de Autenticacion del usuario";
 		require_once '../vistas/login.php';
 	}
 	   
-	/*if($docente->findBy("CEDULA = $cedula")){
-		//print $docente->getNombre()."<br>";
-	}*/	
+
+}
+
+if($_POST['procesar'] == "Setear"){
+	
+	if($_POST['password1'] == $_POST['password2']){
+		$filas = $usuario->setDefaultPassword($_POST['password1']);
+		$tipo_msg="info";
+		$mensaje="Se resetearon $filas registros";		
+	}else{
+		$tipo_msg="error";
+		$mensaje="Errro los password no coinciden";
+		
+	}
+	require_once '../vistas/setear_password_profesores.php';
 }
 ?>
